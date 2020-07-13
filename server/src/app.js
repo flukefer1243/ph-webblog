bbb563e538095d089a99b80a17e5146ade972f34
flukefer1243 <flukefer1243@gmail.com>
2020-07-13 12:10:21 +0700

    folder perpared

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,14 @@
 let express = require('express')
 const app = express()
 let bodyParser = require('body-parser')
+const {sequelize} = require('./models')
+
+const config = require('./config/config')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
+
+require('./routes')(app)
 
 app.get('/status',function (req,res){
     res.send('Hello nodejs server')
@@ -38,8 +43,9 @@ app.delete('/user/:userId', function (req, res) {
         JSON.stringify(req.body))
    })
 
-let port = 8081
-
-app.listen(port, function (){
-    console.log('server running on '+ port)
+let port = process.env.PORT || config.port
+sequelize.sync({force: false}).then(() => {
+   app.listen(port, function () {
+      console.log('Server running on ' + port)
+   })
 })
