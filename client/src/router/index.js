70764f9144929d6f275e96dc32a84a137401919e
flukefer1243 <flukefer1243@gmail.com>
2020-07-23 09:06:50 +0700

    folder perpared

---
+++ client/src/router/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+import UserIndex from '@/components/Users/Index'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/Users',
+      name: 'users',
+      component: UserIndex
+    }
+  ]
+})
